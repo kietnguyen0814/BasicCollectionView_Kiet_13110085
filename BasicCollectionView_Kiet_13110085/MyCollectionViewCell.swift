3b7f738eba0d79aed9a6a26fa3d3983ec41ac3bc
Nguyễn Anh Kiệt <kietnguyen0814@gmail.com>
2017-04-28 23:17:52 +0700

    Basic Collection View final

---
+++ BasicCollectionView_Kiet_13110085/MyCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MyCollectionViewCell.swift
+//  BasicCollectionView_Kiet_13110085
+//
+//  Created by Kiet Nguyen on 4/28/17.
+//  Copyright © 2017 Kiet Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var myImageView: UIImageView!
+}
--- BasicCollectionView_Kiet_13110085/ViewController.swift
+++ BasicCollectionView_Kiet_13110085/ViewController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
+    
+    @IBOutlet weak var MyCollectionView: UICollectionView!
+    var images = ["1", "2", "3", "4", "1", "2", "3", "4", "1"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,23 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionCell", for: indexPath) as! MyCollectionViewCell
+        
+        //set images
+        cell.myImageView.image = UIImage(named: images[indexPath.row])
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("Selected row is", indexPath.row)
+    }
 
 }
 
